11df0834e6d09dd2fbca1c9932058b79e0240f87
manjogit <31913932+manjogit@users.noreply.github.com>
2017-11-21 00:11:13 +0100

    Update Begriff.java

--- alexadeka/src/main/java/dekaskill/Begriff.java
+++ alexadeka/src/main/java/dekaskill/Begriff.java
@@ -6,13 +6,18 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName="Begriffe")
 public class Begriff {
-	private String begriff,text;
+	private String begriff,text,topic,title,imageURL,imageURLSmall,url;
 	
 	public Begriff(){}
 	
-	public Begriff(String begriff, String text){
+	public Begriff(String begriff, String text, String title, String topic, String imageURL, String imageURLSmall, String url){
 		this.begriff=begriff;
 		this.text=text;
+		this.title=title;
+		this.topic=topic;
+		this.imageURL=imageURL;
+		this.imageURLSmall=imageURLSmall;
+		this.imageURL=url;
 	}
 	
     @DynamoDBHashKey(attributeName="begriff")
@@ -22,5 +27,25 @@ public class Begriff {
 	@DynamoDBAttribute(attributeName="text")  
 	public String getText() {return text;}
 	public void setText(String text) {this.text = text;}
+	
+	@DynamoDBAttribute(attributeName="topic")  
+	public String getTopic() {return topic;}
+	public void setTopic(String topic) {this.topic = topic;}
+	
+	@DynamoDBAttribute(attributeName="title")  
+	public String getTitle() {return title;}
+	public void setTitle(String title) {this.title = title;}
+	
+	@DynamoDBAttribute(attributeName="imageURL")  
+	public String getImageURL() {return imageURL;}
+	public void setImageURL(String imageURL) {this.imageURL = imageURL;}
+	
+	@DynamoDBAttribute(attributeName="imageURLSmall")  
+	public String getImageURLSmall() {return imageURLSmall;}
+	public void setImageURLSmall(String imageURLSmall) {this.imageURLSmall = imageURLSmall;}
+	
+	@DynamoDBAttribute(attributeName="url")  
+	public String getUrl() {return url;}
+	public void setUrl(String url) {this.url = url;}
 	
 }
